16bc0f129f3ea0df1a7ee402f62a6cc340e8d8ce
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-01 03:41:28 +0600

    feat(Module-1/Input): demonstrate taking input in C++ and typecasting to show ASCII values

--- Week-1(Introduction to C++)/Module-1(Basic c++)/input.cpp
+++ Week-1(Introduction to C++)/Module-1(Basic c++)/input.cpp
@@ -1,22 +1,22 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     int x = 10, y=65;
-//     char c = 'a';
-//     double d = 1.1; 
+int main(){
+    int x = 10, y=65;
+    char c = 'a';
+    double d = 1.1; 
 
-//     cin >> x >> c >> d;
-//     cout << x << endl << d << endl;
+    cin >> x >> c >> d;
+    cout << x << endl << d << endl;
 
-//     // way-1(Normal)
-//     int ascii = c;
-//     cout << c << " er ascii value: " << ascii << "\n";
+    // way-1(Normal)
+    int ascii = c;
+    cout << c << " er ascii value: " << ascii << "\n";
 
-//     // way -2 (Typecasting)
-//     cout << c << " er ascii value: " << int(c) << endl;
+    // way -2 (Typecasting)
+    cout << c << " er ascii value: " << int(c) << endl;
 
-//     cout << y << " " << char(y);
+    cout << y << " " << char(y);
 
-//     return 0;
-// }
+    return 0;
+}
