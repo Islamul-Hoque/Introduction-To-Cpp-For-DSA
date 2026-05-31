c1e512cec63f80e99450f04f31162f632fc93647
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-01 03:48:16 +0600

    feat(Week-1/Module-1/Min_Max_Swap.cpp): demonstrate min, max functions and swap operations

--- Week-1(Introduction to C++)/Module-1(Basic c++)/Min_Max_Swap.cpp
+++ Week-1(Introduction to C++)/Module-1(Basic c++)/Min_Max_Swap.cpp
@@ -1,28 +1,28 @@
-// #include <iostream>
-// #include <algorithm>
-// using namespace std;
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
-// int main(){
-//     int a, b;
-//     cin >> a >> b;
+int main(){
+    int a, b;
+    cin >> a >> b;
 
-//     // min
-//     if(a<b) cout << a << endl;
-//     else  cout << b << endl;
+    // min
+    if(a<b) cout << a << endl;
+    else  cout << b << endl;
 
-//     // build-in min(), max() func
-//     cout << "Min is: " << min(a, b) << endl;
-//     cout << "Max is: " << max(a, b) << endl;
-//     cout << "Max is: " << max({a, b, 4, 6, 7, 12}) << endl;
+    // build-in min(), max() func
+    cout << "Min is: " << min(a, b) << endl;
+    cout << "Max is: " << max(a, b) << endl;
+    cout << "Max is: " << max({a, b, 4, 6, 7, 12}) << endl;
 
-//     // manual swap
-//     int temp =a;
-//     a=b;
-//     b = temp;
+    // manual swap
+    int temp =a;
+    a=b;
+    b = temp;
 
-//     // Swap() func
-//     swap(a, b);
+    // Swap() func
+    swap(a, b);
 
-//     cout << a << " " << b;
+    cout << a << " " << b;
 
-// }
+}
