bdc5bd65762ca04ac7ad976dfcca8409f630f336
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-01 03:42:49 +0600

    feat(Module-1/Setprecision): demonstrate controlling floating-point output using fixed and setprecision

--- Week-1(Introduction to C++)/Module-1(Basic c++)/Setprecision.cpp
+++ Week-1(Introduction to C++)/Module-1(Basic c++)/Setprecision.cpp
@@ -1,11 +1,11 @@
-// #include <iostream>
-// #include <iomanip>
-// using namespace std;
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
-// int main(){
-//     double x = 123.1234567;
+int main(){
+    double x = 123.1234567;
 
-//     cout << fixed << setprecision(7) << x;
+    cout << fixed << setprecision(7) << x;
 
-//     return 0;
-// }
+    return 0;
+}
