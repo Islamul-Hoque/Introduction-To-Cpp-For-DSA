24ae0e212460e82f54f38613cf627b8215190682
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-01 03:43:50 +0600

    feat(Module-1/Ternary-Operator): use ternary operator to determine even or odd numbers

--- Week-1(Introduction to C++)/Module-1(Basic c++)/Ternary_Operator.cpp
+++ Week-1(Introduction to C++)/Module-1(Basic c++)/Ternary_Operator.cpp
@@ -1,10 +1,10 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     int x = 14;
+int main(){
+    int x = 14;
 
-//     (x%2==0) ? cout << "Even" : cout << "Odd";
+    (x%2==0) ? cout << "Even" : cout << "Odd";
 
-//     return 0;
-// }
+    return 0;
+}
