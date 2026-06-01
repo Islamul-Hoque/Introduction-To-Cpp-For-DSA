e3e31688a62b7c8c9b2a17799c7a705403d01d40
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-02 03:59:13 +0600

    feat(Module-2/Dynamic-Array): implement static vs dynamic array program

--- Week-1(Introduction to C++)/Module-2(Dynamic_Memory_Allocation)/Dynamic_Array.cpp
+++ Week-1(Introduction to C++)/Module-2(Dynamic_Memory_Allocation)/Dynamic_Array.cpp
@@ -1,16 +1,16 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
 
-// int main() {
-//     int a[5]; // Static Array
-//     int *a = new int [5]; // Dynamic Array
+int main() {
+    int a[5]; // Static Array
+    int *a = new int [5]; // Dynamic Array
 
-//     for (int i = 0; i <5; i++)
-//         cin >> a[i];
+    for (int i = 0; i <5; i++)
+    cin >> a[i];
 
-//     for (int i = 0; i <5; i++)
-//         cout << a[i] << " ";
+    for (int i = 0; i <5; i++)
+        cout << a[i] << " ";
 
-//     return 0;
-// }
+    return 0;
+}
