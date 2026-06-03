35d48a3478a6e4f98302f2fb75847567c60cdcca
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-04 01:05:36 +0600

    feat(Module-3/Sorting): add program demonstrating array sorting in ascending and descending order using STL sort()

--- Week-1(Introduction to C++)/Module-3(Class and Object in C++)/Sorting.cpp
+++ Week-1(Introduction to C++)/Module-3(Class and Object in C++)/Sorting.cpp
@@ -1,19 +1,19 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cin >> n;
-//     int a[n];
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
 
-//     for (int i = 0; i <n; i++)
-//         cin >> a[i];
+    for (int i = 0; i <n; i++)
+        cin >> a[i];
 
-//     sort(a, a+n);  // ascending
-//     sort(a, a+n, greater<int>());  // descending
+    sort(a, a+n);  // ascending
+    sort(a, a+n, greater<int>());  // descending
 
-//     for (int i = 0; i <n; i++)
-//         cout << a[i] << " ";
+    for (int i = 0; i <n; i++)
+        cout << a[i] << " ";
 
-//     return 0;
-// }
+    return 0;
+}
