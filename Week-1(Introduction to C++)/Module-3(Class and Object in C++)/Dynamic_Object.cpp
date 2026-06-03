cc91e50a9b81995b7d432815f4fa37753525a402
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-04 00:57:18 +0600

    feat(Module-3/Dynamic-Object): add program demonstrating dynamic object creation using 'new' keyword and accessing data via pointer

--- Week-1(Introduction to C++)/Module-3(Class and Object in C++)/Dynamic_Object.cpp
+++ Week-1(Introduction to C++)/Module-3(Class and Object in C++)/Dynamic_Object.cpp
@@ -1,23 +1,23 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Student {
-//     public:   
-//     int roll;
-//     int cls;
-//     double gpa;
+class Student {
+    public:   
+    int roll;
+    int cls;
+    double gpa;
 
-//     // Constructors
-//     Student(int roll, int cls, double gpa){
-//         this->roll = roll;
-//         this->cls = cls;
-//         this->gpa = gpa;
-//     }
-// };
+    // Constructors
+    Student(int roll, int cls, double gpa){
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
 
-// int main(){
-//     Student* p = new Student(20, 11, 3.9);
-//     cout << "Nayeem: " << p->roll << " " << p->cls << " " << p->gpa << endl;
+int main(){
+    Student* p = new Student(20, 11, 3.9);
+    cout << "Nayeem: " << p->roll << " " << p->cls << " " << p->gpa << endl;
 
-//     return 0;
-// }
+    return 0;
+}
