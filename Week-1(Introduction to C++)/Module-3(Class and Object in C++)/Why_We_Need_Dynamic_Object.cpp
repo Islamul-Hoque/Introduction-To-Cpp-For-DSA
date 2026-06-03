36efd6d229bd9cf8022c2a78daa1f908ec6fc278
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-04 00:55:53 +0600

    feat(Module-3/Why-We-Need-Dynamic-Object): add program demonstrating returning object pointer from function using constructor and accessing data via pointer

--- Week-1(Introduction to C++)/Module-3(Class and Object in C++)/Why_We_Need_Dynamic_Object.cpp
+++ Week-1(Introduction to C++)/Module-3(Class and Object in C++)/Why_We_Need_Dynamic_Object.cpp
@@ -1,29 +1,29 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Student {
-//     public:   
-//     int roll;
-//     int cls;
-//     double gpa;
+class Student {
+    public:   
+    int roll;
+    int cls;
+    double gpa;
 
-//     // Constructors
-//     Student(int roll, int cls, double gpa){
-//         this->roll = roll;
-//         this->cls = cls;
-//         this->gpa = gpa;
-//     }
-// };
+    // Constructors
+    Student(int roll, int cls, double gpa){
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
 
-// Student* fun(){
-//     Student nayeem(20, 11, 3.9);
-//     Student* p = &nayeem;
-//     return p;
-// }
+Student* fun(){
+    Student nayeem(20, 11, 3.9);
+    Student* p = &nayeem;
+    return p;
+}
 
-// int main(){
-//     Student* p = fun();
-//     cout << "Nayeem: " << p->roll << " " << p->cls << " " << p->gpa << endl;
+int main(){
+    Student* p = fun();
+    cout << "Nayeem: " << p->roll << " " << p->cls << " " << p->gpa << endl;
 
-//     return 0;
-// }
+    return 0;
+}
