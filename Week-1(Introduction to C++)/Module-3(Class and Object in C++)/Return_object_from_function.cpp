22e790f07b9b117e1019cb8240fea865a0251567
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-04 00:52:57 +0600

    feat(Module-3/Return-object-from-function): add program demonstrating returning a class object from function and printing object data

--- Week-1(Introduction to C++)/Module-3(Class and Object in C++)/Return_object_from_function.cpp
+++ Week-1(Introduction to C++)/Module-3(Class and Object in C++)/Return_object_from_function.cpp
@@ -1,29 +1,29 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Student {
-//     public:   
-//     int roll;
-//     int cls;
-//     double gpa;
+class Student {
+    public:   
+    int roll;
+    int cls;
+    double gpa;
 
-//     // Constructors
-//     Student(int roll, int cls, double gpa){
-//         // this->roll = roll;   // this-> or (*this).
-//         (*this).roll = roll;   
-//         this->cls = cls;
-//         this->gpa = gpa;
-//     }
-// };
+    // Constructors
+    Student(int roll, int cls, double gpa){
+        // this->roll = roll;   // this-> or (*this).
+        (*this).roll = roll;   
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
 
-// Student fun(){
-//     Student nayeem(20, 11, 3.9);
-//     return nayeem;
-// }
+Student fun(){
+    Student nayeem(20, 11, 3.9);
+    return nayeem;
+}
 
-// int main(){
-//     Student obj = fun();
-//     cout << "Nayeem: " << obj.roll << " " << obj.cls << " " << obj.gpa << endl;
+int main(){
+    Student obj = fun();
+    cout << "Nayeem: " << obj.roll << " " << obj.cls << " " << obj.gpa << endl;
 
-//     return 0;
-// }
+    return 0;
+}
