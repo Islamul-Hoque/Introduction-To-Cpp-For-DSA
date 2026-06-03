f937402b68d8a52bb82b6dd1925c8fbcec13e56e
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-04 00:50:23 +0600

    feat(Module-3/This-and-Arrow): add program demonstrating use of 'this' pointer in C++ constructor for object initialization

--- Week-1(Introduction to C++)/Module-3(Class and Object in C++)/This_and_Arrow.cpp
+++ Week-1(Introduction to C++)/Module-3(Class and Object in C++)/This_and_Arrow.cpp
@@ -1,25 +1,25 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Student {
-//     public:   
-//     int roll;
-//     int cls;
-//     double gpa;
+class Student {
+    public:   
+    int roll;
+    int cls;
+    double gpa;
 
-//     // Constructors
-//     Student(int roll, int cls, double gpa){
-//         // this->roll = roll;   // this-> or (*this).
-//         (*this).roll = roll;   
-//         this->cls = cls;
-//         this->gpa = gpa;
-//     }
-// };
+    // Constructors
+    Student(int roll, int cls, double gpa){
+        // this->roll = roll;   // this-> or (*this).
+        (*this).roll = roll;   
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
 
-// int main(){
-//     Student nayeem(20, 11, 3.9);
+int main(){
+    Student nayeem(20, 11, 3.9);
 
-//     cout << "Nayeem: " << nayeem.roll << " " << nayeem.cls << " " << nayeem.gpa << endl;
+    cout << "Nayeem: " << nayeem.roll << " " << nayeem.cls << " " << nayeem.gpa << endl;
 
-//     return 0;
-// }
+    return 0;
+}
