55d1d4e7e62225f1f8df60187325999f25bcaa0f
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-04 01:14:50 +0600

    feat(Week-1/Practice): solve 'Simple Calculator' problem by reading two integers and printing sum, product, and difference with proper formatting

--- Week-1(Introduction to C++)/Practice Day-1(Module-2.5)/C_Simple_Calculator.cpp
+++ Week-1(Introduction to C++)/Practice Day-1(Module-2.5)/C_Simple_Calculator.cpp
@@ -1,13 +1,13 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     long long int x, y;
-//     cin >> x >> y;
+int main(){
+    long long int x, y;
+    cin >> x >> y;
 
-//     cout << x << " + " << y << " = " << x+y << endl;
-//     cout << x << " * " << y << " = " << x*y << endl;
-//     cout << x << " - " << y << " = " << x-y << endl;
+    cout << x << " + " << y << " = " << x+y << endl;
+    cout << x << " * " << y << " = " << x*y << endl;
+    cout << x << " - " << y << " = " << x-y << endl;
 
-//     return 0;
-// }
+    return 0;
+}
