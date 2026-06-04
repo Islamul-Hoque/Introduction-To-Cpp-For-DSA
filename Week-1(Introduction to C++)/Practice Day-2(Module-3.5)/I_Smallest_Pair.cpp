1aa6a6eafe2f17a1c5fa7ad5379a573afdbc3b99
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-05 01:51:25 +0600

    feat(Practice-3.5/I_Smallest_Pair.cpp): solve 'Smallest Pair' problem by finding minimum value of Ai + Aj + (j - i) using brute-force nested loops

--- Week-1(Introduction to C++)/Practice Day-2(Module-3.5)/I_Smallest_Pair.cpp
+++ Week-1(Introduction to C++)/Practice Day-2(Module-3.5)/I_Smallest_Pair.cpp
@@ -1,26 +1,26 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int t;
-//     cin >> t;
+int main(){
+    int t;
+    cin >> t;
 
-//     // EOF
-//     while(t--){
-//     int n;
-//     cin >> n;
+    // EOF
+    while(t--){
+    int n;
+    cin >> n;
 
-//     int a[n];
-//     for (int i = 0; i <n; i++)
-//         cin >> a[i];
+    int a[n];
+    for (int i = 0; i <n; i++)
+        cin >> a[i];
 
-//     // Min check
-//     int res = INT_MAX;
-//     for (int i = 0; i <n; i++)
-//         for (int j = i+1; j <n; j++)
-//             res = min((a[i] + a[j] + (j - i)), res);
-//     cout << res << endl;
-//     }
+    // Min check
+    int res = INT_MAX;
+    for (int i = 0; i <n; i++)
+        for (int j = i+1; j <n; j++)
+            res = min((a[i] + a[j] + (j - i)), res);
+    cout << res << endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
