6afc9aba951dbb6acf1985800448fb2512d51624
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-05 01:22:11 +0600

    feat(Practice-2.5/E_Max.cpp): solve 'Max' problem by finding maximum element in array using loop and std::max

--- Week-1(Introduction to C++)/Practice Day-1(Module-2.5)/E_Max.cpp
+++ Week-1(Introduction to C++)/Practice Day-1(Module-2.5)/E_Max.cpp
@@ -1,19 +1,19 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cin >> n;
-//     int a[n];
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
 
-//     for (int i = 0; i <n; i++)
-//     cin >> a[i];
+    for (int i = 0; i <n; i++)
+    cin >> a[i];
 
-//     int mx = INT_MIN;
-//     for (int i = 0; i <n; i++)
-//         mx = max(a[i], mx);
+    int mx = INT_MIN;
+    for (int i = 0; i <n; i++)
+        mx = max(a[i], mx);
 
-//     cout << mx;
+    cout << mx;
 
-//     return 0;
-// }
+    return 0;
+}
