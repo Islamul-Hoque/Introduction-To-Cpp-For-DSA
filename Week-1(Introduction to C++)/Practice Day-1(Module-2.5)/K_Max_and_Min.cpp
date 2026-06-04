8b36bc580b68951962cae4719399a2ccd1037c09
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-05 01:23:03 +0600

    feat(Practice-2.5/K_Max_and_Min.cpp): solve 'Max and Min' problem by printing minimum and maximum among three integers using std::min and std::max

--- Week-1(Introduction to C++)/Practice Day-1(Module-2.5)/K_Max_and_Min.cpp
+++ Week-1(Introduction to C++)/Practice Day-1(Module-2.5)/K_Max_and_Min.cpp
@@ -1,14 +1,14 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int a, b, c;
-//     cin >> a >> b >> c;
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
 
-//     int maximum = max({a, b, c});
-//     int minimum = min({a, b, c});
+    int maximum = max({a, b, c});
+    int minimum = min({a, b, c});
 
-//     cout << minimum << " " << maximum;
+    cout << minimum << " " << maximum;
 
-//     return 0;
-// }
+    return 0;
+}
