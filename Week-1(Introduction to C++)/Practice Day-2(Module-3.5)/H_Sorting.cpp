28f6a9c85b53bd8c059842a91d82ae9fee61cfbf
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-05 01:58:12 +0600

    feat(Practice-3.5/H_Sorting.cpp): solve 'Sorting' problem by implementing manual selection/bubble sort without using built-in functions

--- Week-1(Introduction to C++)/Practice Day-2(Module-3.5)/H_Sorting.cpp
+++ Week-1(Introduction to C++)/Practice Day-2(Module-3.5)/H_Sorting.cpp
@@ -1,33 +1,33 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cin >> n;
-//     int a[n];
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
 
-//     for (int i = 0; i <n; i++)
-//     cin >> a[i];
+    for (int i = 0; i <n; i++)
+    cin >> a[i];
 
-//     // way-1: Manual Sort (Selection/Bubble)
-//     for (int i = 0; i <n; i++)
-//         for (int j = i+1; j <n; j++){
-//             if(a[i]>a[j]){
-//                 // manual swap
-//                 int temp =a[i];
-//                 a[i] = a[j];
-//                 a[j] = temp;
+    // way-1: Manual Sort (Selection/Bubble)
+    // for (int i = 0; i <n; i++)
+    //     for (int j = i+1; j <n; j++){
+    //         if(a[i]>a[j]){
+    //             // manual swap
+    //             int temp =a[i];
+    //             a[i] = a[j];
+    //             a[j] = temp;
 
-//                 // built-in swap() func
-//                 swap(a[i], a[j]);
-//             }
-//         }
+    //             // built-in swap() func
+    //             swap(a[i], a[j]);
+    //         }
+    //     }
 
-//     // way-2: STL sort (built‑in) | Standard Template Library
-//     sort(a, a+n);
+    // way-2: STL sort (built‑in) | Standard Template Library
+    sort(a, a+n);
 
-//     for (int i = 0; i <n; i++)
-//         cout << a[i] << " ";
+    for (int i = 0; i <n; i++)
+        cout << a[i] << " ";
 
-//     return 0;
-// }
+    return 0;
+}
