83776d46dc842dadd0b9521a34e5f5ac9461f0ad
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-06 15:30:37 +0600

    feat(Mid-Exam/Monkey): solve 'Monkey' problem by reading input until EOF, removing spaces, sorting lowercase letters in ascending order, and printing result

--- Assignment/Mid Exam/Monkey.cpp
+++ Assignment/Mid Exam/Monkey.cpp
@@ -1,22 +1,22 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main() {
-//     char s[100001];
+int main() {
+    char s[100001];
 
-//     // EOF
-//     while (cin.getline(s, 100001)) {
-//         int len = strlen(s);
+    // EOF
+    while (cin.getline(s, 100001)) {
+        int len = strlen(s);
 
-//         string letters;
-//         for (int i = 0; i < len; i++) 
-//             if (s[i] != ' ') letters.push_back(s[i]);  // Removing space
+        string letters;
+        for (int i = 0; i < len; i++) 
+            if (s[i] != ' ') letters.push_back(s[i]);  // Removing space
 
-//         // ascending order
-//         sort(letters.begin(), letters.end());
+        // ascending order
+        sort(letters.begin(), letters.end());
 
-//         cout << letters << endl;
-//     }
+        cout << letters << endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
