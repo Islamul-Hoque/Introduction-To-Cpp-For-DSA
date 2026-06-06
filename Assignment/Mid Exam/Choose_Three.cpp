db2d8800d5839ef8bf2cad56ec84e1abbeb5ad6f
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-06 15:33:41 +0600

    feat(Mid-Exam/Choose-Three): solve 'Choose Three' problem by checking all distinct triplets in array to determine if their sum equals given S

--- Assignment/Mid Exam/Choose_Three.cpp
+++ Assignment/Mid Exam/Choose_Three.cpp
@@ -1,31 +1,31 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int t;
-//     cin >> t;
+int main(){
+    int t;
+    cin >> t;
 
-//     while (t--) {
-//         // 5 times n,s a[n] input
-//         int n, s, flag = 0;;
-//         cin >> n >> s;
+    while (t--) {
+        // 5 times n,s a[n] input
+        int n, s, flag = 0;;
+        cin >> n >> s;
 
-//         int a[n];
-//         for (int i = 0; i <n; i++) cin >> a[i];
+        int a[n];
+        for (int i = 0; i <n; i++) cin >> a[i];
 
-//         // 3 index check for 5 times
-//         for (int i = 0; i <n; i++){
-//             for (int j = i+1; j <n; j++){
-//                 for (int k = j+1; k <n; k++)
-//                     if(a[i]+a[j]+ a[k] == s) {
-//                         flag = 1;
-//                         break;
-//                     }
-//             }
-//         }
-//         if(flag == 1 ) cout << "YES" << endl;
-//         else cout << "NO" << endl;
-//     }
+        // 3 index check for 5 times
+        for (int i = 0; i <n; i++){
+            for (int j = i+1; j <n; j++){
+                for (int k = j+1; k <n; k++)
+                    if(a[i]+a[j]+ a[k] == s) {
+                        flag = 1;
+                        break;
+                    }
+            }
+        }
+        if(flag == 1 ) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
