f1a7961b31b0717186b41653dd2986a7dc99afe8
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-06 15:32:55 +0600

    feat(Mid-Exam/Who-Is-It): solve 'Who Is It' problem by implementing class Student, reading multiple test cases, finding highest marks, and resolving ties using smallest ID

--- Assignment/Mid Exam/Who_Is_It.cpp
+++ Assignment/Mid Exam/Who_Is_It.cpp
@@ -1,39 +1,39 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Student {
-//     public:
-//     int ID;
-//     char name[101];
-//     char section;
-//     int total_marks;
-// };
+class Student {
+    public:
+    int ID;
+    char name[101];
+    char section;
+    int total_marks;
+};
 
-// int main(){
-//     int t;
-//     cin >> t;
+int main(){
+    int t;
+    cin >> t;
 
-//     while (t--) {
-//         Student arr[3];
+    while (t--) {
+        Student arr[3];
 
-//         for (int i = 0; i <3; i++)
-//             cin >> arr[i].ID >> arr[i].name >> arr[i].section >> arr[i].total_marks;
+        for (int i = 0; i <3; i++)
+            cin >> arr[i].ID >> arr[i].name >> arr[i].section >> arr[i].total_marks;
 
-//         // Default best student
-//         Student highest_marks = arr[0];
+        // Default best student
+        Student highest_marks = arr[0];
 
-//         for (int i = 1; i < 3; i++) {
-//             // Find max Mark
-//             if( arr[i].total_marks > highest_marks.total_marks) highest_marks = arr[i];
+        for (int i = 1; i < 3; i++) {
+            // Find max Mark
+            if( arr[i].total_marks > highest_marks.total_marks) highest_marks = arr[i];
 
-//             // Marks same => ID check
-//             else if (arr[i].total_marks == highest_marks.total_marks) {
-//                 if(arr[i].ID < highest_marks.ID) highest_marks = arr[i];
-//             }
-//         }
+            // Marks same => ID check
+            else if (arr[i].total_marks == highest_marks.total_marks) {
+                if(arr[i].ID < highest_marks.ID) highest_marks = arr[i];
+            }
+        }
 
-//         cout << highest_marks.ID << " " << highest_marks.name << " " << highest_marks.section << " " << highest_marks.total_marks << endl;
-//     }
+        cout << highest_marks.ID << " " << highest_marks.name << " " << highest_marks.section << " " << highest_marks.total_marks << endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
