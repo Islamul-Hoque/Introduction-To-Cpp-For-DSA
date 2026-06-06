3c6cfe1f76914dc48c9b5451dfff089de4c093ac
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-06 15:27:49 +0600

    feat(Mid-Exam/Sort-It): solve 'Sort It' problem by implementing array sorting in ascending and descending order using STL sort()

---
+++ Assignment/Mid Exam/Monkey.cpp
@@ -0,0 +1,22 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     char s[100001];
+
+//     // EOF
+//     while (cin.getline(s, 100001)) {
+//         int len = strlen(s);
+
+//         string letters;
+//         for (int i = 0; i < len; i++) 
+//             if (s[i] != ' ') letters.push_back(s[i]);  // Removing space
+
+//         // ascending order
+//         sort(letters.begin(), letters.end());
+
+//         cout << letters << endl;
+//     }
+
+//     return 0;
+// }
--- Week-1(Introduction to C++)/Practice Day-2(Module-3.5)/H_Sorting.cpp
+++ Week-1(Introduction to C++)/Practice Day-2(Module-3.5)/H_Sorting.cpp
@@ -10,18 +10,18 @@ int main(){
     cin >> a[i];
 
     // way-1: Manual Sort (Selection/Bubble)
-    // for (int i = 0; i <n; i++)
-    //     for (int j = i+1; j <n; j++){
-    //         if(a[i]>a[j]){
-    //             // manual swap
-    //             int temp =a[i];
-    //             a[i] = a[j];
-    //             a[j] = temp;
+    for (int i = 0; i <n; i++)
+        for (int j = i+1; j <n; j++){
+            if(a[i]>a[j]){
+                // manual swap
+                int temp =a[i];
+                a[i] = a[j];
+                a[j] = temp;
 
-    //             // built-in swap() func
-    //             swap(a[i], a[j]);
-    //         }
-    //     }
+                // built-in swap() func
+                swap(a[i], a[j]);
+            }
+        }
 
     // way-2: STL sort (built‑in) | Standard Template Library
     sort(a, a+n);
