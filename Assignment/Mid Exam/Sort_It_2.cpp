4396c04fb5d70164e26a3931a0346dfc8c7ab9cf
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-06 15:29:35 +0600

    feat(Mid-Exam/Sort-It-2): solve 'Sort It 2' problem by implementing sort_it() function with dynamic array input, descending order sorting, and returning array to main

--- Assignment/Mid Exam/Sort_It_2.cpp
+++ Assignment/Mid Exam/Sort_It_2.cpp
@@ -1,22 +1,22 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int* sort_it(int n){
-//     int* a = new int[n];
+int* sort_it(int n){
+    int* a = new int[n];
 
-//     for (int i = 0; i <n; i++)
-//         cin >> a[i];
+    for (int i = 0; i <n; i++)
+        cin >> a[i];
 
-//     sort(a, a+n, greater<int>());
-//     return a;
-// }
+    sort(a, a+n, greater<int>());
+    return a;
+}
 
-// int main(){
-//     int n;
-//     cin >> n;
-//     int* a = sort_it(n);
+int main(){
+    int n;
+    cin >> n;
+    int* a = sort_it(n);
 
-//     for (int i = 0; i <n; i++)
-//         cout << a[i] << " ";
-//     return 0;
-// }
+    for (int i = 0; i <n; i++)
+        cout << a[i] << " ";
+    return 0;
+}
