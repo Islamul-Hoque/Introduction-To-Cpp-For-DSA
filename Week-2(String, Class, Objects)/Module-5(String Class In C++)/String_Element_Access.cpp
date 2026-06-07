8a5fe6ee5a08e0fb5cdd06bbf70026d0276dbe50
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-07 16:43:53 +0600

    feat(Week-2/String_Element_Access.cpp): add program demonstrating string element access using [], at(), front(), and back()

--- Week-2(String, Class, Objects)/Module-5(String Class In C++)/String_Element_Access.cpp
+++ Week-2(String, Class, Objects)/Module-5(String Class In C++)/String_Element_Access.cpp
@@ -1,21 +1,21 @@
-// // Element access
-// #include <bits/stdc++.h>
-// using namespace std;
+// Element access
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     string s = "HELLO";
+int main(){
+    string s = "HELLO";
 
-//     // ith index
-//     cout << s[3] << endl;
-//     cout << s.at(3) << endl; // cross
+    // ith index
+    cout << s[3] << endl;
+    cout << s.at(3) << endl; // cross
 
-//     // first element
-//     cout << s.front() << endl; // cross
-//     cout << s[0] << endl;
+    // first element
+    cout << s.front() << endl; // cross
+    cout << s[0] << endl;
 
-//     // last element
-//     cout << s.back() << endl;
-//     cout << s[s.size()-1] << endl;  // cross
+    // last element
+    cout << s.back() << endl;
+    cout << s[s.size()-1] << endl;  // cross
 
-//     return 0;
-// }
+    return 0;
+}
