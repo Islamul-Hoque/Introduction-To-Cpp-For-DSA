f425f6e64e539a1eb609e0f5a02c166defca8c81
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-08 16:52:34 +0600

    feat(Week-2/String_Modifiers.cpp): add program demonstrating string concatenation, append, push_back, and pop_back operations

--- Week-2(String, Class, Objects)/Module-5(String Class In C++)/String_Modifiers.cpp
+++ Week-2(String, Class, Objects)/Module-5(String Class In C++)/String_Modifiers.cpp
@@ -1,31 +1,33 @@
-// // String Modifiers
-// #include <bits/stdc++.h>
-// using namespace std;
+// String Modifiers
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     // C-style string concatenation
-//     char c[50] = "Islamul Hoque";
-//     char c2[50] = " Ishfak";
+int main(){
+    // C-style string concatenation
+    char c[50] = "Islamul Hoque";
+    char c2[50] = " Ishfak";
 
-//     strcat(c, c2);
-//     cout << c << endl;
+    strcat(c, c2);
+    cout << c << endl;
 
-//     // c++ built-in operator
-//     string s = "Islamul Hoque";
-//     string s2 = " Ishfak";
+    // c++ built-in operator
+    string s = "Islamul Hoque";
+    string s2 = " Ishfak";
 
-//     // way-1
-//     s += s2;
-//     // way-2
-//     s.append(s2);  // cross
+    // way-1
+    s += s2;
+    // way-2
+    s.append(s2);  // cross
 
-//     // add character to the last of the string
-//     s.push_back('A');  // cross
-//     s += 'B';
+    // add character to the last of the string
+    // way-1
+    s.push_back('A');  // cross
+    // way-2
+    s += 'B';
 
-//     // remove the last character of the string
-//     s.pop_back();
-//     cout << s << endl;
+    // remove the last character of the string
+    s.pop_back();
+    cout << s << endl;
 
-//     return 0;
-// }
+    return 0;
+}
