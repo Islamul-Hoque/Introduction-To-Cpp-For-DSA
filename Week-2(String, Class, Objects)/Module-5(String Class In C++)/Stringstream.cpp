b53e3acfc3de96bf989d470530c58c4afdcfe5a9
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-10 17:18:40 +0600

    feat(Week-2/Stringstream.cpp): add program demonstrating string input splitting into words using stringstream

--- Week-2(String, Class, Objects)/Module-5(String Class In C++)/Stringstream.cpp
+++ Week-2(String, Class, Objects)/Module-5(String Class In C++)/Stringstream.cpp
@@ -1,21 +1,21 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     string s;
-//     getline(cin, s);
-//     cout << s << endl;
+int main(){
+    string s;
+    getline(cin, s);
+    cout << s << endl;
 
-//     // string কে word-by-word read
-//     stringstream ss(s);
+    // string কে word-by-word read
+    stringstream ss(s);
 
-//     string word; 
-//     int count =0;
-//     while(ss>> word){ // প্রতিবার (ss >> word) করলে পরবর্তী word পাওয়া যায়
-//             cout << word << endl;
-//             count++;
-//         }
+    string word; 
+    int count =0;
+    while(ss>> word){ // প্রতিবার (ss >> word) করলে পরবর্তী word পাওয়া যায়
+            cout << word << endl;
+            count++;
+        }
 
-//     cout << "Total word: " << count << endl;
-//     return 0;
-// }
+    cout << "Total word: " << count << endl;
+    return 0;
+}
