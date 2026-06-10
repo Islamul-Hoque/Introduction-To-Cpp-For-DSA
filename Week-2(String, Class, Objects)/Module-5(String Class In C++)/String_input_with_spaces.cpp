ccfd66b82ffc79384db39fa3f24c1e104fb35eee
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-10 17:17:16 +0600

    feat(Week-2/String_input_with_spaces.cpp): add program demonstrating string input with spaces using cin.getline() and getline()

--- Week-2(String, Class, Objects)/Module-5(String Class In C++)/String_input_with_spaces.cpp
+++ Week-2(String, Class, Objects)/Module-5(String Class In C++)/String_input_with_spaces.cpp
@@ -1,20 +1,20 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int x;
-//     cin >> x;
-//     cin.ignore();  // for ignore enter
+int main(){
+    int x;
+    cin >> x;
+    cin.ignore();  // for ignore enter
 
-//     // c style space input
-//     char c[100];
-//     cin.getline(c, 100);
+    // c style space input
+    char c[100];
+    cin.getline(c, 100);
 
-//     // c ++ build-in class
-//     string s;
-//     getline(cin, s);
+    // c ++ build-in class
+    string s;
+    getline(cin, s);
 
-//     cout << x << endl << c << endl << s << endl;
+    cout << x << endl << c << endl << s << endl;
 
-//     return 0;
-// }
+    return 0;
+}
