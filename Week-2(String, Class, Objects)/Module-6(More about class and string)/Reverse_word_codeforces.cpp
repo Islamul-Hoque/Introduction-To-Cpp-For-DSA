b77211e191c80b7e4e4fae9fd84294686934538e
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-11 17:43:57 +0600

    feat(Module-6/Reverse_word_codeforces.cpp): add program reversing each word in a string using stringstream and reverse()

--- Week-2(String, Class, Objects)/Module-6(More about class and string)/Reverse_word_codeforces.cpp
+++ Week-2(String, Class, Objects)/Module-6(More about class and string)/Reverse_word_codeforces.cpp
@@ -1,23 +1,23 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     string s;
-//     getline(cin, s);
+int main(){
+    string s;
+    getline(cin, s);
 
-//     stringstream ss(s);
-//     string word;
+    stringstream ss(s);
+    string word;
 
-//     // reverse and print first word (no space)
-//     ss>> word;
-//     reverse(word.begin(), word.end());
-//     cout << word;
+    // reverse and print first word (no space)
+    ss>> word;
+    reverse(word.begin(), word.end());
+    cout << word;
 
-//     while (ss >> word){
-//         // reverse and print remaining words (with leading space)
-//         reverse(word.begin(), word.end());
-//         cout << " " << word;
-//     }
+    while (ss >> word){
+        // reverse and print remaining words (with leading space)
+        reverse(word.begin(), word.end());
+        cout << " " << word;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
