01842b40df51499e83b13a039a674818b2d0f600
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-11 17:36:37 +0600

    feat(Module-5/Sort_String.cpp): add program demonstrating string character sorting using sort()

--- Week-2(String, Class, Objects)/Module-6(More about class and string)/Sort_String.cpp
+++ Week-2(String, Class, Objects)/Module-6(More about class and string)/Sort_String.cpp
@@ -1,12 +1,12 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     string s;
-//     cin >> s;
+int main(){
+    string s;
+    cin >> s;
 
-//     sort(s.begin(), s.end());
-//     cout << s << endl;
+    sort(s.begin(), s.end());
+    cout << s << endl;
 
-//     return 0;
-// }
+    return 0;
+}
