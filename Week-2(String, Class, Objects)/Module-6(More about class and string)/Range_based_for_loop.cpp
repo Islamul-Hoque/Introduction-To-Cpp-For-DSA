9edd67f0636a96aa2fd8508e1df1125759cbeaae
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-11 17:37:49 +0600

    feat(Module-5/Range_based_for_loop.cpp): add program demonstrating string iteration using index-based and range-based for loops

--- Week-2(String, Class, Objects)/Module-6(More about class and string)/Range_based_for_loop.cpp
+++ Week-2(String, Class, Objects)/Module-6(More about class and string)/Range_based_for_loop.cpp
@@ -1,17 +1,17 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     string s;
-//     cin >> s;
+int main(){
+    string s;
+    cin >> s;
 
-//     // index based for Loop 
-//     for (int i = 0; i <s.size(); i++)
-//         cout << s[i] << endl;
+    // index based for Loop 
+    for (int i = 0; i <s.size(); i++)
+        cout << s[i] << endl;
 
-//     // build-in Range based for Loop
-//     for (char c : s)
-//         cout << c << endl;
+    // build-in Range based for Loop
+    for (char c : s)
+        cout << c << endl;
 
-//     return 0;
-// }
+    return 0;
+}
