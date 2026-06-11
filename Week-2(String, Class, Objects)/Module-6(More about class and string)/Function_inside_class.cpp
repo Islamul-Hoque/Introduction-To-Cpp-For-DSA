96a20cc791997b76bf9dac4c354e00cbacfe76e2
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-11 17:46:21 +0600

    feat(Module-6/Function_inside_class.cpp): add Student class with constructor, member functions for total marks and greeting

--- Week-2(String, Class, Objects)/Module-6(More about class and string)/Function_inside_class.cpp
+++ Week-2(String, Class, Objects)/Module-6(More about class and string)/Function_inside_class.cpp
@@ -1,40 +1,40 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Student{
-//     public:
-//     string name;
-//     int roll;
-//     int math;
-//     int english;
+class Student{
+public:
+    string name;
+    int roll;
+    int math;
+    int english;
 
-//     Student(string name, int roll, int math, int english){
-//         this->name = name;
-//         this->roll = roll;
-//         this->math = math;
-//         this->english = english;
-//     }
+    Student(string name, int roll, int math, int english){
+        this->name = name;
+        this->roll = roll;
+        this->math = math;
+        this->english = english;
+    }
 
-//     void Hello(){
-//         cout << "Hello from " << name << endl << endl;
-//     }
+    void Hello(){
+        cout << "Hello from " << name << endl << endl;
+    }
 
-//     void Total(){
-//         cout << "Total marks of" << name << " = " << math+english << endl;
-//     }
+    void Total(){
+        cout << "Total marks of" << name << " = " << math+english << endl;
+    }
 
-// };
+};
 
-// int main(){
-//     Student ishfak("Ishfak", 1, 100, 100);
-//     cout << ishfak.name << " " << ishfak.roll << " " << ishfak.math << " " << ishfak.english << endl;
-//     ishfak.Total();
-//     ishfak.Hello();
+int main(){
+    Student ishfak("Ishfak", 1, 100, 100);
+    cout << ishfak.name << " " << ishfak.roll << " " << ishfak.math << " " << ishfak.english << endl;
+    ishfak.Total();
+    ishfak.Hello();
 
-//     Student nayeem("Nayeem", 1, 100, 100);
-//     cout << nayeem.name << " " << nayeem.roll << " " << nayeem.math << " " << nayeem.english << endl;
-//     nayeem.Total();
-//     nayeem.Hello();
+    Student nayeem("Nayeem", 1, 100, 100);
+    cout << nayeem.name << " " << nayeem.roll << " " << nayeem.math << " " << nayeem.english << endl;
+    nayeem.Total();
+    nayeem.Hello();
 
-//     return 0;
-// }
+    return 0;
+}
