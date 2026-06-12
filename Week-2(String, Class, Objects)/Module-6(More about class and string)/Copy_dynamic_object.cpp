af1a0faa0f7bb6e7f4baf2457884caf9467db8c8
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-12 17:49:14 +0600

    feat(Module-6/Copy_dynamic_object.cpp): add program demonstrating dynamic object creation and correct ways of copying objects

--- Week-2(String, Class, Objects)/Module-6(More about class and string)/Copy_dynamic_object.cpp
+++ Week-2(String, Class, Objects)/Module-6(More about class and string)/Copy_dynamic_object.cpp
@@ -1,33 +1,36 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Cricketer {
-//     public:
-//     string country;
-//     int jersey;
+class Cricketer {
+public:
+    string country;
+    int jersey;
 
-//     Cricketer(string country, int jersey){
-//         this->country = country;
-//         this->jersey = jersey;
-//     }
-// };
+    Cricketer(string country, int jersey){
+        this->country = country;
+        this->jersey = jersey;
+    }
+};
 
-// int main(){
-//     Cricketer* dhoni = new Cricketer("india1", 10);
-//     Cricketer* kholi = new Cricketer("india2", 18);
+int main(){
+    Cricketer* dhoni = new Cricketer("india1", 10);
+    Cricketer* kholi = new Cricketer("india2", 18);
 
-//     // wrong way
-//     kholi = dhoni;
-//     delete dhoni;
-//     cout << kholi->country << " " << kholi->jersey << endl;
-    
-//     // right way-1
-//     kholi->country = dhoni->country;
-//     kholi->jersey = dhoni->jersey;
+    // wrong way
+    kholi = dhoni;
+    delete dhoni;
+    cout << kholi->country << " " << kholi->jersey << endl;
 
-//     // way-2
-//     *kholi = *dhoni;
-//     cout << kholi->country << " " << kholi->jersey << endl;
+    // right way-1
+    kholi->country = dhoni->country;
+    kholi->jersey = dhoni->jersey;
+    delete dhoni;
+    cout << kholi->country << " " << kholi->jersey << endl;
 
-//     return 0;
-// }
+    // way-2
+    *kholi = *dhoni;
+    delete dhoni;
+    cout << kholi->country << " " << kholi->jersey << endl;
+
+    return 0;
+}
