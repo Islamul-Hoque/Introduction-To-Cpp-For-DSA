8b1d2049399a522d51fa38f18b5d97244b1eea41
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-15 01:37:37 +0600

    feat(module-7/Sort_array_of_objects_part_2): implement multiple comparison strategies for sorting student objects by marks and roll

--- Week-2(String, Class, Objects)/Module-7(Array of objects)/Sort_array_of_objects.cpp
+++ Week-2(String, Class, Objects)/Module-7(Array of objects)/Sort_array_of_objects.cpp
@@ -1,32 +1,32 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Student {
-//     public:
-//     string name;
-//     int roll;
-//     int marks;
-// };
+class Student {
+    public:
+    string name;
+    int roll;
+    int marks;
+};
 
-// bool cmp(Student l, Student r){
-//     if(l.marks < r.marks) 
-//         return true;
-//     else 
-//         return false; 
-// }
+bool cmp(Student l, Student r){
+    if(l.marks < r.marks) 
+        return true;
+    else 
+        return false; 
+}
 
-// int main(){
-//     int n;
-//     cin >> n;
-//     Student a[n];
+int main(){
+    int n;
+    cin >> n;
+    Student a[n];
 
-//     for (int i = 0; i < n; i++) 
-//         cin >> a[i].name >> a[i].roll >> a[i].marks;
+    for (int i = 0; i < n; i++) 
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
 
-//     sort(a, a+n, cmp);
+    sort(a, a+n, cmp);
 
-//     for (int i = 0; i < n; i++) 
-//         cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+    for (int i = 0; i < n; i++) 
+        cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
 
-//     return 0;
-// }
+    return 0;
+}
