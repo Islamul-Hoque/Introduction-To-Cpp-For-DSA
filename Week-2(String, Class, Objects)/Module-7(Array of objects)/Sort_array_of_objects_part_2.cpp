2278aa4548be04d186cc1a5bda7d39c8d888435e
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-15 01:36:38 +0600

    feat(module-7/Sort_array_of_objects): implement sorting of student objects by marks using custom comparator function

--- Week-2(String, Class, Objects)/Module-7(Array of objects)/Sort_array_of_objects_part_2.cpp
+++ Week-2(String, Class, Objects)/Module-7(Array of objects)/Sort_array_of_objects_part_2.cpp
@@ -1,42 +1,42 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Student {
-//     public:
-//     string name;
-//     int roll;
-//     int marks;
-// };
+class Student {
+    public:
+    string name;
+    int roll;
+    int marks;
+};
 
-// bool cmp(Student l, Student r){
-//     // main process
-//     if(l.marks >= r.marks) 
-//         return true;
-//     else 
-//         return false; 
+bool cmp(Student l, Student r){
+    // main process
+    if(l.marks >= r.marks) 
+        return true;
+    else 
+        return false; 
 
-//     // shortcut
-//     // mark wise sort
-//     return l.marks < r.marks;
+    // shortcut
+    // mark wise sort
+    return l.marks < r.marks;
 
-//     // roll wise sort ascending 
-//     return l.roll < r.roll;
-//     // roll wise descending sort
-//     return l.roll > r.roll;
-// }
+    // roll wise sort ascending 
+    return l.roll < r.roll;
+    // roll wise descending sort
+    return l.roll > r.roll;
+}
 
-// int main(){
-//     int n;
-//     cin >> n;
-//     Student a[n];
+int main(){
+    int n;
+    cin >> n;
+    Student a[n];
 
-//     for (int i = 0; i < n; i++) 
-//         cin >> a[i].name >> a[i].roll >> a[i].marks;
+    for (int i = 0; i < n; i++) 
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
 
-//     sort(a, a+n, cmp);
+    sort(a, a+n, cmp);
 
-//     for (int i = 0; i < n; i++) 
-//         cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+    for (int i = 0; i < n; i++) 
+        cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
 
-//     return 0;
-// }
+    return 0;
+}
