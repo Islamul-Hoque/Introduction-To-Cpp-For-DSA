ca4c29b48599b2406787b8b9986e745f2414822f
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-17 02:24:58 +0600

    feat(final-exam/Sort_It_Again): sort students by English marks, then Math marks, with ID tie-breaker for consistent ordering

--- Assignment/Final Exam/Sort_It_Again.cpp
+++ Assignment/Final Exam/Sort_It_Again.cpp
@@ -1,33 +1,33 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Students{
-// public:
-//     string name; 
-//     int cls;
-//     char section; 
-//     int id; 
-//     int math_marks;
-//     int eng_marks;
-// };
+class Students{
+public:
+    string name; 
+    int cls;
+    char section; 
+    int id; 
+    int math_marks;
+    int eng_marks;
+};
 
-// bool cmp(Students l, Students r){
-//     return 
-//     (l.eng_marks != r.eng_marks) ? (l.eng_marks > r.eng_marks) :
-//     (l.math_marks != r.math_marks) ? (l.math_marks > r.math_marks) : l.id <r.id;
-// }
+bool cmp(Students l, Students r){
+    return 
+    (l.eng_marks != r.eng_marks) ? (l.eng_marks > r.eng_marks) :
+    (l.math_marks != r.math_marks) ? (l.math_marks > r.math_marks) : l.id <r.id;
+}
 
-// int main(){
-//     int n;
-//     cin >> n;
-//     Students a[n];
+int main(){
+    int n;
+    cin >> n;
+    Students a[n];
 
-//     for (int i = 0; i <n; i++)
-//     cin >> a[i].name >> a[i].cls >> a[i].section >> a[i].id >> a[i].math_marks >> a[i].eng_marks;
+    for (int i = 0; i <n; i++)
+    cin >> a[i].name >> a[i].cls >> a[i].section >> a[i].id >> a[i].math_marks >> a[i].eng_marks;
 
-//     sort(a, a+n, cmp);
-//     for (int i = 0; i <n; i++)
-//     cout << a[i].name << " " << a[i].cls << " " << a[i].section << " " << a[i].id << " " << a[i].math_marks << " " << a[i].eng_marks <<endl;
+    sort(a, a+n, cmp);
+    for (int i = 0; i <n; i++)
+    cout << a[i].name << " " << a[i].cls << " " << a[i].section << " " << a[i].id << " " << a[i].math_marks << " " << a[i].eng_marks <<endl;
 
-//     return 0;
-// }
+    return 0;
+}
